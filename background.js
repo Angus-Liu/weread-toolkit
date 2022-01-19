023097f72b35486ffcd6e572a3d93239391cfc92
Angus-Liu <angus.liu96@gmail.com>
2022-01-19 15:05:40 +0800

    feat: delete background.js

--- background.js
+++
@@ -1,7 +0,0 @@
-chrome.runtime.onInstalled.addListener(() => {
-    chrome.tabs.onActivated.addListener(async info => {
-        let tab = await chrome.tabs.get(info.tabId);
-        let isWeRead = tab.url.startsWith('https://weread.qq.com/');
-        isWeRead ? chrome.action.enable(tab.tabId) : chrome.action.disable(tab.tabId);
-    });
-});
