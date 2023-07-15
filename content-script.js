b1c9f9ee9218d7e3b9301a15426809c4d1c4d712
Angus Liu <angus.liu96@gmail.com>
2023-07-15 10:59:37 +0800

    feat: load last Settings

--- content-script.js
+++ content-script.js
@@ -2,9 +2,9 @@
  * 将 HTML 字符串转换为 Element
  *
  * @param htmlStr 用于转换为 element 的 html 字符串
- * @return ChildNode
+ * @return HTML Element
  */
-function htmlToElement(htmlStr) {
+function htmlToElement (htmlStr) {
     const template = document.createElement('template');
     template.innerHTML = htmlStr.trim();
     return template.content.firstChild;
@@ -13,30 +13,31 @@ function htmlToElement(htmlStr) {
 let fullScreen = false;
 let readerTopBarMaxWidth, appContentMaxWidth, readerControlsLeft;
 
-function runFullScreen() {
+
+/**
+ * 全屏切换
+ */
+function updateScreen () {
     // 阅读页面顶部工具栏
     let readerTopBar = document.querySelector('div.readerTopBar');
     // 内容显示区域
     let appContent = document.querySelector('div.app_content');
     // 左侧控件工具栏
     let readerControls = document.querySelector('div.readerControls');
-
-    fullScreen = !fullScreen;
-
     if (fullScreen) {
+        // 阅读页面顶部工具栏
         readerTopBarMaxWidth = readerTopBar.style.maxWidth;
         readerTopBar.style.maxWidth = 'none';
-
+        // 内容显示区域
         appContentMaxWidth = appContent.style.maxWidth;
         appContent.style.maxWidth = 'none';
-
+        // 左侧控件工具栏
         readerControlsLeft = readerControls.style.left;
         readerControls.style.left = 'unset';
         readerControls.style.right = '1%';
     } else {
         readerTopBar.style.maxWidth = readerTopBarMaxWidth;
         appContent.style.maxWidth = appContentMaxWidth;
-
         readerControls.style.left = readerControlsLeft;
         readerControls.style.right = 'unset';
     }
@@ -45,16 +46,21 @@ function runFullScreen() {
     window.dispatchEvent(new Event('resize'));
 }
 
-
 let retryTimes = 3;
 
-// 将按钮添加到控制栏
-function appendFullScreenItem() {
+/**
+ * 添加全屏按钮
+ */
+function appendFullScreenItem () {
     try {
         let fullScreenItem = htmlToElement(
             '<button title="全屏" class="readerControls_item full"><span class="icon"></span></button>'
         );
-        fullScreenItem.addEventListener('click', runFullScreen);
+        fullScreenItem.addEventListener('click', () => {
+            fullScreen = !fullScreen;
+            updateScreen();
+            chrome.storage.local.set({ fullScreen });
+        });
         // 将按钮添加到控制栏
         let readerControls = document.querySelector('div.readerControls');
         readerControls.prepend(fullScreenItem);
@@ -70,3 +76,9 @@ function appendFullScreenItem() {
 
 // 延迟 2s 执行，避免其他 js 后执行覆盖掉修改
 setTimeout(appendFullScreenItem, 2000);
+
+// 加载历史配置
+chrome.storage.local.get(["fullScreen"]).then((result) => {
+    fullScreen = result.fullScreen;
+    if (fullScreen) updateScreen();
+});
